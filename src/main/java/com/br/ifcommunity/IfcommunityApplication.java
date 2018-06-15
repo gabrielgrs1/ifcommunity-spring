cccb0bd1397797948e910beefe2ebe830a795730
gabrielgrs1 <gabriel_guilherme2006@hotmail.com>
2018-06-15 12:52:35 -0300

    First commit

---
+++ src/main/java/com/br/ifcommunity/IfcommunityApplication.java
@@ -0,0 +1,12 @@
+package com.br.ifcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfcommunityApplication.class, args);
+	}
+}
---
+++ src/test/java/com/br/ifcommunity/IfcommunityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.ifcommunity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IfcommunityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
