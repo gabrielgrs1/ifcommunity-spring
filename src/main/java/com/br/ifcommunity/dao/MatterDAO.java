f16fb6761051e47b7f46ef4838ed8e84486b6525
gabrielgrs1 <gabriel_guilherme2006@hotmail.com>
2018-06-22 00:15:40 -0300

    [FIX] Pattern name var

--- src/main/java/com/br/ifcommunity/dao/MatterDAO.java
+++ src/main/java/com/br/ifcommunity/dao/MatterDAO.java
@@ -2,7 +2,6 @@ package com.br.ifcommunity.dao;
 
 import com.br.ifcommunity.model.Matter;
 import com.br.ifcommunity.model.MatterUser;
-import com.br.ifcommunity.model.User;
 import com.br.ifcommunity.util.ConnectionFactory;
 
 import java.sql.Connection;
@@ -37,7 +36,7 @@ public class MatterDAO {
         return mattersList;
     }
 
-    public static ArrayList<Matter> getUserMatters(int idUser) throws SQLException {
+    public static ArrayList<Matter> getUserMatters(int studentId) throws SQLException {
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
         Connection connection = ConnectionFactory.getConnection();
@@ -48,7 +47,7 @@ public class MatterDAO {
                 " WHERE ID_ALUNO = ?";
 
         preparedStatement = connection.prepareStatement(SQLQuery);
-        preparedStatement.setInt(1, idUser);
+        preparedStatement.setInt(1, studentId);
         resultSet = preparedStatement.executeQuery();
 
         while (resultSet.next()) {
