0fde2f78d526f2a9895076e8e32bcb3d18a6d6f6
gabrielgrs1 <gabriel_guilherme2006@hotmail.com>
2018-06-27 02:08:11 -0300

    [FIX] Security session using token

--- src/main/java/com/br/ifcommunity/dao/ChartDAO.java
+++ src/main/java/com/br/ifcommunity/dao/ChartDAO.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class ChartDAO {
-    public static ArrayList<Chart> getChartsInfo(int userId) throws SQLException {
+    public static ArrayList<Chart> getChartsInfo(String userId) throws SQLException {
         PreparedStatement preparedStatement;
         ResultSet resultSet;
         Connection connection = ConnectionFactory.getConnection();
@@ -21,63 +21,76 @@ public class ChartDAO {
         int deslike = 0;
 
 
-        String SQLQuery = "SELECT TB_POSTAGEM.LINGUAGEM_POSTAGEM, TB_MATERIA.NOME_MATERIA,"
-                + " COUNT(LINGUAGEM_POSTAGEM) as CONTAGEM_POSTAGEM FROM TB_POSTAGEM"
-                + " INNER JOIN TB_MATERIA ON (TB_POSTAGEM.ID_MATERIA = TB_MATERIA.ID)"
-                + " WHERE ID_USUARIO = ? "
-                + " GROUP BY TB_POSTAGEM.LINGUAGEM_POSTAGEM, TB_MATERIA.NOME_MATERIA";
+        String token = userId.split(";")[0];
+        userId = (userId.split(";")[1]);
 
+        String SQLQuery = "SELECT * FROM TB_USUARIO WHERE ID = ? AND TOKEN = ?";
         preparedStatement = Objects.requireNonNull(connection).prepareStatement(SQLQuery);
-        preparedStatement.setInt(1, userId);
+        preparedStatement.setInt(1, Integer.parseInt(userId));
+        preparedStatement.setString(2, token);
         resultSet = preparedStatement.executeQuery();
 
-        while (resultSet.next()) {
-            chart = new Chart(resultSet.getString("LINGUAGEM_POSTAGEM"),
-                    resultSet.getInt("CONTAGEM_POSTAGEM"),
-                    resultSet.getString("NOME_MATERIA")
-            );
 
+        if (resultSet.next()) {
+            SQLQuery = "SELECT TB_POSTAGEM.LINGUAGEM_POSTAGEM, TB_MATERIA.NOME_MATERIA,"
+                    + " COUNT(LINGUAGEM_POSTAGEM) as CONTAGEM_POSTAGEM FROM TB_POSTAGEM"
+                    + " INNER JOIN TB_MATERIA ON (TB_POSTAGEM.ID_MATERIA = TB_MATERIA.ID)"
+                    + " WHERE ID_USUARIO = ? "
+                    + " GROUP BY TB_POSTAGEM.LINGUAGEM_POSTAGEM, TB_MATERIA.NOME_MATERIA";
+
+            preparedStatement = Objects.requireNonNull(connection).prepareStatement(SQLQuery);
+            preparedStatement.setInt(1, Integer.parseInt(userId));
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                chart = new Chart(resultSet.getString("LINGUAGEM_POSTAGEM"),
+                        resultSet.getInt("CONTAGEM_POSTAGEM"),
+                        resultSet.getString("NOME_MATERIA")
+                );
+
+                chartList.add(chart);
+            }
+
+            SQLQuery = "SELECT * FROM TB_CONTAGEM_LIKE " +
+                    " INNER JOIN TB_POSTAGEM TP on TB_CONTAGEM_LIKE.ID_POSTAGEM = TP.ID"
+                    + " WHERE TP.ID_USUARIO = ? ";
+
+            preparedStatement = Objects.requireNonNull(connection).prepareStatement(SQLQuery);
+            preparedStatement.setInt(1, Integer.parseInt(userId));
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                if (resultSet.getInt("LIKE_DESLIKE") == 0) {
+                    like++;
+                } else {
+                    deslike++;
+                }
+            }
+
+            SQLQuery = "SELECT * FROM TB_CONTAGEM_LIKE " +
+                    " INNER JOIN TB_POSTAGEM TP on TB_CONTAGEM_LIKE.ID_POSTAGEM = TP.ID" +
+                    " INNER JOIN TB_COMENTARIO_POSTAGEM TC on TP.ID = TC.ID_POSTAGEM"
+                    + " WHERE TP.ID_USUARIO = ? ";
+
+            preparedStatement = Objects.requireNonNull(connection).prepareStatement(SQLQuery);
+            preparedStatement.setInt(1, Integer.parseInt(userId));
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                if (resultSet.getInt("LIKE_DESLIKE") == 0) {
+                    like++;
+                } else {
+                    deslike++;
+                }
+            }
+
+            chart = new Chart(like, deslike);
             chartList.add(chart);
+
+
         }
-
-        SQLQuery = "SELECT * FROM TB_CONTAGEM_LIKE " +
-                " INNER JOIN TB_POSTAGEM TP on TB_CONTAGEM_LIKE.ID_POSTAGEM = TP.ID"
-                + " WHERE TP.ID_USUARIO = ? ";
-
-        preparedStatement = Objects.requireNonNull(connection).prepareStatement(SQLQuery);
-        preparedStatement.setInt(1, userId);
-        resultSet = preparedStatement.executeQuery();
-
-        while (resultSet.next()) {
-            if (resultSet.getInt("LIKE_DESLIKE") == 0) {
-                like++;
-            } else {
-                deslike++;
-            }
-        }
-
-        SQLQuery = "SELECT * FROM TB_CONTAGEM_LIKE " +
-                " INNER JOIN TB_POSTAGEM TP on TB_CONTAGEM_LIKE.ID_POSTAGEM = TP.ID" +
-                " INNER JOIN TB_COMENTARIO_POSTAGEM TC on TP.ID = TC.ID_POSTAGEM"
-                + " WHERE TP.ID_USUARIO = ? ";
-
-        preparedStatement = Objects.requireNonNull(connection).prepareStatement(SQLQuery);
-        preparedStatement.setInt(1, userId);
-        resultSet = preparedStatement.executeQuery();
-
-        while (resultSet.next()) {
-            if (resultSet.getInt("LIKE_DESLIKE") == 0) {
-                like++;
-            } else {
-                deslike++;
-            }
-        }
-
-        chart = new Chart(like, deslike);
-        chartList.add(chart);
 
         connection.close();
-
         return chartList;
     }
 }
